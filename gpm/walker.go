d2d9d2d4ae502aa41a501e72b70f5618afc93f66
yayanyang <yayanyang@gmail.com>
2018-10-09 14:23:58 +0800

    update

--- gpm/walker.go
+++ gpm/walker.go
@@ -16,31 +16,15 @@ type walkerImpl struct {
 
 // NewWalker create package walker object
 func NewWalker() Walker {
-	return &walkerImpl{
-		Logger: slf4go.Get("package-walker"),
-	}
+	return &walkerImpl{}
 }
 
-func (impl *walkerImpl) imported(abspath string) (*build.Package, bool) {
+func (impl *walkerImpl) imported(key string) (*build.Package, bool) {
 
-	for _, pkg := range impl.packages {
-		if pkg.ImportPath == abspath {
-			return pkg, true
-		}
-	}
-
-	return nil, false
 }
 
-func (impl *walkerImpl) importedDir(dir string) (*build.Package, bool) {
+func (impl *walkerImpl) importedDir(key string) (*build.Package, bool) {
 
-	for _, pkg := range impl.packages {
-		if pkg.Dir == dir {
-			return pkg, true
-		}
-	}
-
-	return nil, false
 }
 
 func (impl *walkerImpl) Import(dir string, recursion bool) error {
@@ -51,14 +35,15 @@ func (impl *walkerImpl) Import(dir string, recursion bool) error {
 		return xerrors.Wrapf(err, "get fullpath of %s error", dir)
 	}
 
-	pkg, ok := impl.importedDir(path)
+	_, ok := impl.importedDir(path)
 
 	if ok {
-		impl.DebugF("get imported package: %s", pkg.ImportPath)
 		return nil
 	}
 
-	pkg, err = impl.importDir(dir)
+	var fifo []string
+
+	pkg, err := impl.importDir(dir)
 
 	if err != nil {
 		return xerrors.Wrapf(err, "get package from dir %s error", dir)
@@ -66,18 +51,13 @@ func (impl *walkerImpl) Import(dir string, recursion bool) error {
 
 	impl.packages = append(impl.packages, pkg)
 
-	impl.DebugF("find package: %s", pkg.ImportPath)
-
 	if !recursion {
 		return nil
 	}
 
-	var fifo []string
-
 	fifo = append(fifo, pkg.Imports...)
 
 	for len(fifo) > 0 {
-
 		top := fifo[0]
 
 		fifo = fifo[1:]
@@ -88,16 +68,8 @@ func (impl *walkerImpl) Import(dir string, recursion bool) error {
 			continue
 		}
 
-		pkg, err := impl.importPackage(top)
+		
 
-		if err != nil {
-			impl.DebugF("get imported package: %s", pkg.ImportPath)
-			return err
-		}
-
-		impl.packages = append(impl.packages, pkg)
-
-		impl.DebugF("find package: %s", pkg.ImportPath)
 	}
 
 	return nil
